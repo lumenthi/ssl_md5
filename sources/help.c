11ad0c2ef8e98cc759241dc44f808f25b82034d7
lumenthi <lumenthi@student.42.fr>
2022-12-20 12:17:15 +0100

    :children_crossing: Art for help menu

--- sources/help.c
+++ sources/help.c
@@ -1,8 +1,27 @@
 #include "../headers/ssl.h"
 
+static void print_art()
+{
+	printf(
+	"\n"
+	"\n"
+    "     ███████╗████████╗     ███████╗███████╗██╗        \n"
+    "     ██╔════╝╚══██╔══╝     ██╔════╝██╔════╝██║        \n"
+    "     █████╗     ██║        ███████╗███████╗██║        \n"
+    "     ██╔══╝     ██║        ╚════██║╚════██║██║        \n"
+    "     ██║        ██║███████╗███████║███████║███████╗   \n"
+    "     ╚═╝        ╚═╝╚══════╝╚══════╝╚══════╝╚══════╝ 1.0.1\n"
+    "                                   lumenthi@student.42.fr\n"
+	"\n"
+	);
+}
+
 void print_version()
 {
-	ft_putstr("Version menu\n");
+	printf("lumenthi's ft_ssl version 1.0.1 (https://github.com/lumenthi/ssl_md5)\n"
+			"This program is free software; you may redistribute it\n"
+			"This program has absolutely no warranty\n"
+	);
 }
 
 void print_usage()
@@ -14,7 +33,15 @@ void print_usage()
 
 void print_help()
 {
+	/* Header */
+	print_art();
 	print_usage();
 	printf("\n");
+
+	/* Content */
 	printf("Help menu\n");
+
+	/* Footer */
+	printf("\n");
+	print_version();
 }
